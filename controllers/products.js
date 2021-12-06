dc449c8059fde8055b19eb1d62be50e98c9fc39d
AlejandroBusi <alejandro_busi@hotmail.com>
2021-12-05 21:25:58 -0300

    fix controllers

--- controllers/products.js
+++ controllers/products.js
@@ -10,23 +10,20 @@ const createProduct = async( req,res ) => {
       return res.status(400).json({ errors: errors.array() });
     }
 
-  const { name, description, category, price, author, stock, isbn, editorial, fav, imgUrl} = req.body
+  const { name, description, category, price, stock, imgUrl, quantity} = req.body
 
   const newProduct = new Product({
     name,
     description,
     category,
     price,
-    author,
     stock,
-    isbn,
-    editorial,
-    fav,
     imgUrl,
+    quantity
   })
   
   await newProduct.save()
-  res.json(`product "${newProduct.name}" with isbn ${newProduct.isbn} created`) 
+  res.json(`product "${newProduct.name}" with ID ${newProduct._id} created`) 
   
   } catch (error) {
     res.json(`something has failed. error : ${error}`) 
@@ -42,14 +39,14 @@ const getProducts = async(req,res) =>{
 
 const deleteProduct = async ( req, res ) => {
   
-  const { isbn } = req.body
+  const { id } = req.body
   const errors = validationResult(req);
   
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  await Product.deleteOne({ isbn: isbn });
-  res.json(`it was successfully eliminated ${isbn} `)
+  await Product.deleteOne({ _id: id });
+  res.json(`it was successfully eliminated ${id}` , res.status)
 }
 
 const editProduct = async ( req, res ) => {
